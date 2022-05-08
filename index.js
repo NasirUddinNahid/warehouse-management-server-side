092246f41a722e7a4be7b673bfd06ffe5118888e
NasirUddinNahid <nahidahmed17762@gmail.com>
2022-05-08 11:39:51 +0600

    verify token

--- index.js
+++ index.js
@@ -7,26 +7,12 @@ const cors = require('cors')
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(express.json());
+app.use(cors())
 
 
 
-function verifyToken(req, res, next) {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-        return res.status(401).send({ message: 'Unauthorized access' })
-    }
-
-    const token = authHeader?.split(' ')[1]
-    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden' })
-        }
-        req.decoded = decoded;
-
-    })
-    next()
-}
 
 const uri = `mongodb+srv://${process.env.DB_userName}:${process.env.SECRET_KEY_mongoDB}@cluster0.ezv3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
